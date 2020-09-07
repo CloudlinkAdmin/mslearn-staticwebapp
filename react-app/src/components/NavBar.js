efb7864cb679c578ac90bc81883b3133ca4b19bc
CloudlinkAdmin <60818101+CloudlinkAdmin@users.noreply.github.com>
2020-09-07 22:10:22 +0200

    updates

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = (props) => (
         Products
       </NavLink>
       <NavLink to="/about" activeClassName="active-link">
-        About
+        About me
       </NavLink>
     </ul>
     {props.children}
